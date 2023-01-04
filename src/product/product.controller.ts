7bd335d89bff34e2a492507c255f61c32b0829e0
EKE Princewill <ekecardinal@gmail.com>
2023-01-04 18:50:09 +0000

    first commit

---
+++ src/product/product.controller.ts
@@ -0,0 +1,49 @@
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
+import { ProductDocument } from './product.schema';
+import { ProductService } from './product.service';
+
+@Controller('product')
+export class ProductController {
+  constructor(private productService: ProductService) {}
+
+  @Post()
+  createProduct(
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description?: string,
+  ) {
+    return this.productService.create(name, price, description);
+  }
+
+  @Get()
+  findAllProducts(): Promise<ProductDocument[]> {
+    return this.productService.findAll();
+  }
+  @Get(':id')
+  findAllProduct(@Param('id') id: string): Promise<ProductDocument> {
+    return this.productService.find(id);
+  }
+
+  @Patch(':id')
+  updateProduct(
+    @Param('id') id: string,
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description?: string,
+  ): Promise<ProductDocument> {
+    return this.productService.update(id, name, price, description);
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id') id: string) {
+    return this.productService.delete(id);
+  }
+}
